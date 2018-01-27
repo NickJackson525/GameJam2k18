6a8ffa9f271b3a02d3c092f654ae943fa04ea7d7
Rernokk <ssmith37@uccs.edu>
2018-01-26 19:31:44 -0700

    Camera Lerping from anchor to anchor.

---
+++ GameJam2k18Project/Assets/Scripts/Camera_Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour {
+
+  [SerializeField] Transform playerTransform;
+	void Start () {
+    playerTransform = GameObject.FindGameObjectWithTag("Start").transform;
+	}
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Mouse0)) {
+      Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+      RaycastHit2D inf = Physics2D.Raycast(r.origin, r.direction);
+      if (inf.transform != null && (inf.transform.tag == "WifiPoint" || inf.transform.tag == "Start")){
+        playerTransform = inf.transform;
+      }
+    }
+  }
+
+  private void LateUpdate()
+  {
+    Vector3 tarPos = playerTransform.position;
+    tarPos.z = transform.position.z;
+    transform.position = Vector3.Lerp(transform.position, tarPos, .15f);
+  }
+}
