85ae49e39ff921b8c23643bb9b83edee235e3c03
LAPTOP-H5PPJI5D\njack <njacksonco@gmail.com>
2018-01-27 14:36:02 -0700

    modified robot script

--- GameJam2k18Project/Assets/Scripts/Robot.cs
+++ GameJam2k18Project/Assets/Scripts/Robot.cs
@@ -4,17 +4,56 @@ using UnityEngine;
 
 public class Robot : MonoBehaviour
 {
-  protected bool isSelected = false;
-  public bool IsSelected
-  {
-    get
+    public bool isSelected { get; set; }
+    protected bool isGrounded;
+    protected float speed = 1f;
+    protected float jumpHeight = 3f;
+    Rigidbody2D rigidbody;
+    Vector2 moveV = new Vector2(0, 3);
+
+    private void Start()
     {
-      return isSelected;
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    set
+    private void Update()
     {
-      isSelected = value;
+
     }
-  }
+
+    protected virtual void MoveLeftRight()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidbody.velocity = new Vector2(-speed, rigidbody.velocity.y);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+        }
+        else
+        {
+            rigidbody.velocity = new Vector2(0, rigidbody.velocity.y);
+        }
+    }
+
+    protected virtual void MoveUpDown()
+    {
+        if (Input.GetKey(KeyCode.W) && rigidbody.velocity.y < moveV.y / 1.5)
+        {
+            rigidbody.velocity += (moveV / 6) / 2;
+        }
+        if (Input.GetKey(KeyCode.S) && rigidbody.velocity.y > -moveV.y)
+        {
+            rigidbody.velocity -= (moveV / 7) / 2;
+        }
+    }
+
+    protected virtual void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpHeight);
+        }
+    }
 }
