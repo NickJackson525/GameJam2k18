3f8591e50c0b3a78742052b346c8fd747144472d
Rernokk <ssmith37@uccs.edu>
2018-01-26 20:03:26 -0700

    Upgrade handles added.

--- GameJam2k18Project/Assets/Scripts/Camera_Controller.cs
+++ GameJam2k18Project/Assets/Scripts/Camera_Controller.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Camera_Controller : MonoBehaviour {
+public class Camera_Controller : MonoBehaviour
+{
 
   [SerializeField] Transform playerTransform;
-	void Start () {
+  [SerializeField] float jumpRangeLimit = 5f;
+  [SerializeField] int rangeUpgradeLevel = 0, delayUpgradeLevel = 0;
+  int[] rangeUpgrades = { 0, 3, 6, 9, 12 };
+  float[] delayUpgrades = { 0f, .05f, .15f, .25f, .4f };
+
+  bool chargingJump = false;
+
+  void Start()
+  {
     playerTransform = GameObject.FindGameObjectWithTag("Start").transform;
-	}
+  }
 
   private void Update()
   {
-    if (Input.GetKeyDown(KeyCode.Mouse0)) {
+    if (Input.GetKeyDown(KeyCode.Mouse0))
+    {
       Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
       RaycastHit2D inf = Physics2D.Raycast(r.origin, r.direction);
-      if (inf.transform != null && (inf.transform.tag == "WifiPoint" || inf.transform.tag == "Start")){
-        playerTransform = inf.transform;
+      if (!chargingJump && inf.transform != null && (inf.transform.tag == "WifiPoint" || inf.transform.tag == "Start") && Vector2.Distance(inf.transform.position, playerTransform.position) < jumpRangeLimit + rangeUpgrades[rangeUpgradeLevel])
+      {
+        print("Jumping to " + inf.transform.name);
+        StartCoroutine(ChargeJump(inf.transform));
       }
     }
   }
@@ -25,5 +37,14 @@ public class Camera_Controller : MonoBehaviour {
     Vector3 tarPos = playerTransform.position;
     tarPos.z = transform.position.z;
     transform.position = Vector3.Lerp(transform.position, tarPos, .15f);
+  }
+
+  private IEnumerator ChargeJump(Transform target)
+  {
+    chargingJump = true;
+    yield return new WaitForSeconds(.8f - delayUpgrades[delayUpgradeLevel]);
+    print("Charging to " + target.name);
+    playerTransform = target;
+    chargingJump = false;
   }
 }
