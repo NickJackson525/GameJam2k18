4f196fd1a3acb13b947609653a2440b23609954e
AustinBorth <aborth@uccs.edu>
2018-01-28 14:15:59 -0700

    Logic game finished

---
+++ GameJam2k18Project/Assets/Scripts/Logic Game Scripts/LogicManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LogicManager : MonoBehaviour {
+
+    [SerializeField]
+    GameObject button1;
+    [SerializeField]
+    GameObject button2;
+    [SerializeField]
+    GameObject button3;
+
+    float timer = 0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (button1.GetComponent<Button>().state && button2.GetComponent<Button>().state
+            && button3.GetComponent<Button>().state)
+        {
+            timer += Time.deltaTime;
+            if(timer > 1.5f)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+
+        }
+        else if(button1.GetComponent<Button>().state == false && button2.GetComponent<Button>().state == false
+            && button3.GetComponent<Button>().state)
+        {
+
+        }
+
+	}
+}
---
+++ GameJam2k18Project/Assets/Scripts/Logic Game Scripts/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public bool testing = false;
+
+    float testtimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(testtimer > 0)
+        {
+            testing = false;
+            testtimer = 0;
+        }
+		if (testing)
+        {
+            testtimer += Time.deltaTime;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        testing = true;
+    }
+}
