0e970a78f8ca7408444ced0f9c5a0676be34fbd3
LAPTOP-H5PPJI5D\njack <njacksonco@gmail.com>
2018-01-27 21:16:06 -0700

    fixed shit

---
+++ GameJam2k18Project/Assets/Scripts/PlayerFromTerminal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFromTerminal : MonoBehaviour
+{
+    public Vector3 targetPoint;
+    public Vector2 startPoint;
+    public int timer = 0;
+    public bool returnHome = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(timer > 0)
+        {
+            timer--;
+
+            if(timer == 0)
+            {
+                returnHome = true;
+            }
+        }
+
+        if(!returnHome)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPoint, .2f);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, startPoint, .2f);
+        }
+	}
+}
