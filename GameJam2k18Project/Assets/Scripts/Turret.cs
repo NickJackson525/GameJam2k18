1812c06667bb6be7bf33bcbe7b876ee9ee594347
Kevin Moore <zyronn.sorrow@gmail.com>
2018-01-27 17:31:17 -0700

    Added shield-turret quotes when possessing

--- GameJam2k18Project/Assets/Scripts/Turret.cs
+++ GameJam2k18Project/Assets/Scripts/Turret.cs
@@ -43,6 +43,7 @@ public class Turret : Robot
     private int rotLeft = 0;
     private int rotRight = 0;
     private IEnumerator coroutine;
+    private bool initialPossess = false;
     #endregion
 
     #region Methods - MonoBehaviour
@@ -60,6 +61,11 @@ public class Turret : Robot
     {
         if(isSelected)
         {
+            if(!initialPossess)
+            {
+                initialPossess = true;
+                PlayRandomSound();
+            }
             StopCoroutine(coroutine);
             // mouse logic
             if(Input.GetKey(KeyCode.A))
@@ -117,6 +123,40 @@ public class Turret : Robot
         projectile.GetComponent<Rigidbody2D>().AddForce(new Vector2(velocityDir.x, velocityDir.y) * projectileSpeed);
         sprite.sprite = firingSprite;
         StartCoroutine(SpriteSwitchRoutine());
+    }
+
+    private void PlayRandomSound()
+    {
+        switch (Random.Range(1, 10))
+        {
+            case 1:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret1);
+                break;
+            case 2:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret2);
+                break;
+            case 3:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret3);
+                break;
+            case 4:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret4);
+                break;
+            case 5:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret5);
+                break;
+            case 6:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret6);
+                break;
+            case 7:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret7);
+                break;
+            case 8:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret8);
+                break;
+            case 9:
+                Audio_Manager.Instance.PlaySound(Audio_Manager.Sound.Turret9);
+                break;
+        }
     }
     #endregion
 
